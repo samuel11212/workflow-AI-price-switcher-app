9d2f749ea51c872397f1c05506d2931cc9eef831
sam11212 <samuellegesse11212@gmail.com>
2023-04-23 23:02:17 +0300

    first message

---
+++ src/Data.jsx
@@ -0,0 +1,52 @@
+const DATA = [
+    {
+        id : 0,
+        title : "Price for 100 Documents Pack",
+        price: 69,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/workflow.JPG"
+    },
+    {
+        id : 1,
+        title : "Price for 1000 Documents Pack",
+        price: 621,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/workflow1.JPG"
+    },
+    {
+        id : 2,
+        title : "Price for 2000 Documents Pack",
+        price: 1173,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/workflow2.JPG"
+    },
+    {
+        id : 3,
+        title : "1-5 members",
+        price: 208,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/workflow3.JPG"
+        
+    },
+    {
+        id : 4,
+        title : "6-10 members",
+        price: 347,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/workflow4.JPG"
+    },
+    {
+        id : 5,
+        title : "11-100 members",
+        price: 625,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/workflow5.JPG"
+    },
+    {
+        id: 6,
+        title: "Development Charges for One Template",
+        price : 347
+    }
+]
+
+export default DATA;
